285913397ff068f2ed7ecbdcb1a52a20e7392b3c
Jannik Vogel <email@jannikvogel.de>
2017-12-27 03:23:57 +0100

    wip doxygen

--- main.h
+++ main.h
@@ -2,6 +2,11 @@
 // Licensed under GPLv2 or any later version
 // Refer to the included LICENSE.txt file.
 
+/** 
+ * @file
+ * This file blabla
+ */
+
 #ifndef __OPENSWE1R_MAIN_H__
 #define __OPENSWE1R_MAIN_H__
 
@@ -24,7 +29,16 @@ static inline int hacky_printf(const char* fmt, ...) {
 Address CreateInterface(const char* name, unsigned int slotCount);
 void AddExport(const char* name, void* callback, Address address);
 
-// Defines an INITIALIZER macro which will run code at startup
+/**
+ * Defines an INITIALIZER macro which will run code at startup
+ * It can be used like this:
+ *
+ * ```
+ * INITIALIZER(name) {
+ *   printf("This function is ran before main!\n");
+ * }
+ * ```
+ */
 #if defined(__GNUC__)
 #  define INITIALIZER(_name) \
     __attribute__((constructor)) void _name()
@@ -47,6 +61,33 @@ void AddExport(const char* name, void* callback, Address address);
 #  error Compiler not detected
 #endif
 
+/**
+ * This macro creates a function which will be registered in the list of exported API functions at startup.
+ * Such functions will later be resolved when an exe is loaded.
+ * The created function will retrieve register values of the virtual CPU to variables.
+ * It will also pop 4 bytes from the stack.
+ * 
+ * The following variables will be available in functions created using this macro:
+ *
+ * - `uint32_t* stack`, a pointer to the stack
+ * - `Address stackAddress`, the address of the stack in guest memory on function entry
+ * - `Address returnAddress`, the value of stack[0]
+ * - `uint32_t eax`, the value of the EAX register, typically used for return values
+ * - `uint32_t eip`, the value of the EIP register, the instruction pointer
+ * - `uint32_t esp`, the value of the ESP register, the stack pointer
+ *
+ * The value of `eip` will be equal to `returnAddress`.
+ * If the function should return elsewhere, `eip` should be be modified.
+ *
+ * Functions started by `HACKY_IMPORT_BEGIN` should be ended with `HACKY_IMPORT_END` like this:
+ *
+ * ```
+ * HACKY_IMPORT_BEGIN(name)
+ *   printf("Function 'name' was called. Parameter was: 0x%08" PRIX32 "\n", stack[1]);
+ *   printf("Returning to 0x%08" PRIX32 "\n", returnAddress);
+ * HACKY_IMPORT_END()
+ * ```
+ */
 #define HACKY_IMPORT_BEGIN(_name) \
   static void Hook_ ## _name (void* uc, Address _address, void* _user_data); \
   INITIALIZER(Register_ ## _name) { \
@@ -78,6 +119,10 @@ void AddExport(const char* name, void* callback, Address address);
     eip = returnAddress; \
     esp += 4; \
 
+/**
+ * This macro ends a function previously started with `HACKY_IMPORT_BEGIN`.
+ * It is responsible for printing debug information and writing CPU register variables back to the virtual CPU.
+ */
 #define HACKY_IMPORT_END() \
     if (!silent) { \
       hacky_printf("Stack at 0x%" PRIX32 "; returning EAX: 0x%08" PRIX32 "\n", stackAddress, eax); \
