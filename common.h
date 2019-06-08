3c5d3eaeef6d9bf0c6d66cafbd917c02c86969fe
Jannik Vogel <email@jannikvogel.de>
2019-06-08 16:25:08 +0200

    Switch to NtAllocateVirtualMemory as default allocator on Xbox (was ContiguousMemory)

--- common.h
+++ common.h
@@ -38,7 +38,11 @@ static uint32_t AlignUp(uint32_t address, uint32_t size) {
 static void* aligned_malloc(size_t alignment, size_t size) {
   void* ptr;
 #ifdef XBOX
-  ptr = MmAllocateContiguousMemoryEx(size, 0x00000000, 0xFFFFFFFF, alignment, PAGE_READWRITE);
+//  ptr = MmAllocateContiguousMemoryEx(size, 0x00000000, 0xFFFFFFFF, alignment, PAGE_READWRITE);
+  ptr = NULL;
+  DWORD allocated_size = size;
+  NTSTATUS status = NtAllocateVirtualMemory(&ptr, 0, &allocated_size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+  assert(status == STATUS_SUCCESS);
 #elif defined(_WIN32)
   ptr = _aligned_malloc(size, alignment);
 #else
@@ -52,7 +56,10 @@ static void* aligned_malloc(size_t alignment, size_t size) {
 
 static void aligned_free(void* ptr) {
 #ifdef XBOX
-  MmFreeContiguousMemory(ptr);
+//  MmFreeContiguousMemory(ptr);
+  DWORD dwSize = 0;
+  NTSTATUS status = NtFreeVirtualMemory(&ptr, &dwSize, MEM_RELEASE);
+  assert(status == STATUS_SUCCESS);
 #elif defined(_WIN32)
   _aligned_free(ptr);
 #else
